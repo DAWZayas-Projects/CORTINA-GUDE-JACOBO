2a3863d8cbe286200d80a9d436b505397d51fb53
Jacobo Cortina <jacobcg@gmail.com>
2017-05-16 19:23:15 +0200

    added connect4 and n_puzzle classes

---
+++ server/api/games/connect_4.py
@@ -0,0 +1,166 @@
+from server.api.algorithms.adversial import minimax_decision, minimax_alfa_beta_decision
+
+class Connect4_Game:
+
+    def __init__(self):
+        self.rows = 6
+        self.columns = 7
+        self.status = 0
+        self.depth = 4
+        self.score = 100000
+        self.player = 0
+        self.board = []
+
+    def generate_board(self):
+        self.board = [[8 for x in range(self.columns)] for y in range(self.rows)]
+
+    def is_finished(self, depth, score, board):
+        if (depth == 0 or score == self.score or score == -self.score or Connect4_Game.is_board_full(self, board)):
+            return True
+        return False
+
+    def is_board_full(self, board):
+        for i in range(self.columns):
+            if (board[0][i] == 8):
+                return False
+        return True
+
+    def change_turn(self):
+        self.player = 1 if self.player == 0 else 0
+
+    def change_player(self, player):
+        return 1 if player == 0 else 0
+
+    def make_move(self, column, board):
+        if (self.board[0][column] == 8 and column >= 0 and column < self.columns):
+            for y in range(self.rows - 1, -1, -1):
+                if (board[y][column] == 8):
+                    board[y][column] = self.player
+                    break
+            return True
+        else:
+            return False
+
+    def make_internal_move(self, column, board, player):
+        if (self.board[0][column] == 8 and column >= 0 and column < self.columns):
+            for y in range(self.rows - 1, -1, -1):
+                if (board[y][column] == 8):
+                    board[y][column] = player
+                    break
+            return True
+        else:
+            return False
+
+    def ai_movement(self):
+        if not Connect4_Game.is_game_over(self):
+            # ai_move = minimax_decision(self, (self.board, self.player), self.depth)
+            ai_move = minimax_alfa_beta_decision(self, (self.board, self.player), self.depth)
+            self.board = ai_move
+            Connect4_Game.change_turn(self)
+            Connect4_Game.print_board(self, self.board)
+            # Connect4_Game.make_move(self, ai_move, self.board)
+
+    def player_movement(self):
+        if not Connect4_Game.is_game_over(self):
+            player_move = input("Column move?")
+            player_move = int(player_move)
+            Connect4_Game.make_move(self, player_move, self.board)
+            Connect4_Game.change_turn(self)
+            Connect4_Game.print_board(self, self.board)
+
+    def children_boards(self, board, player):
+        children = []
+        for column in range(self.columns):
+            new_board = self.clone_board(board)
+            if (Connect4_Game.make_internal_move(self, column, new_board, player)):
+                children.append(new_board)
+        return children
+
+    def clone_board(self, board):
+        return [row[:] for row in board]
+
+    def print_board(self, board):
+        for i in range(self.rows):
+            print(board[i])
+        print("----------------------------------------------------")
+
+    def start_game(self):
+        Connect4_Game.generate_board(self)
+        Connect4_Game.print_board(self, self.board)
+        while self.status == 0:
+            Connect4_Game.player_movement(self)
+            Connect4_Game.ai_movement(self)
+
+    def is_game_over(self):
+        score = Connect4_Game.board_score(self, self.board)
+        if (score == -self.score):
+            self.status = 1
+            print("You have won!")
+            return True
+        if (score == self.score):
+            self.status = 2
+            print("Computer won!")
+            return True
+        if (Connect4_Game.is_board_full(self, self.board)):
+            self.status = 3
+            print("Tie!")
+            return True
+        return False
+
+    def score_position(self, board, row, column, delta_y, delta_x):
+        human_points = 0
+        computer_points = 0
+        for i in range(4):
+            if (board[row][column] == 0):
+                human_points += 1
+            elif (board[row][column] == 1):
+                computer_points += 1
+            row += delta_y
+            column += delta_x
+        if (human_points == 4):
+            return -self.score
+        elif (computer_points == 4):
+            return self.score
+        else:
+            return computer_points
+
+    def board_score(self, board):
+        points = 0
+        vertical_points = 0
+        horizontal_points = 0
+        diagonal_points1 = 0
+        diagonal_points2 = 0
+        for row in range(self.rows - 3):
+            for column in range(self.columns):
+                score = Connect4_Game.score_position(self, board, row, column, 1, 0)
+                if (score == self.score):
+                    return self.score
+                if (score == -self.score):
+                    return -self.score
+                vertical_points += score
+        for row in range(self.rows):
+            for column in range(self.columns - 3):
+                score = Connect4_Game.score_position(self, board, row, column, 0, 1)
+                if (score == self.score):
+                    return self.score
+                if (score == -self.score):
+                    return -self.score
+                horizontal_points += score
+        for row in range(self.rows - 3):
+            for column in range(self.columns - 3):
+                score = Connect4_Game.score_position(self, board, row, column, 1, 1)
+                if (score == self.score):
+                    return self.score
+                if (score == -self.score):
+                    return -self.score
+                diagonal_points1 += score
+        for row in range(3, self.rows):
+            for column in range(self.columns - 3):
+                score = Connect4_Game.score_position(self, board, row, column, -1, 1)
+                if (score == self.score):
+                    return self.score
+                if (score == -self.score):
+                    return -self.score
+                diagonal_points2 += score
+        points = horizontal_points + vertical_points + diagonal_points1 + diagonal_points2
+        return points
---
+++ server/api/games/n_puzzle.py
@@ -0,0 +1,90 @@
+from server.api.utils.node import Node
+from server.api.algorithms.uninformed_search import breadth_first_search
+from server.api.algorithms.informed_search import a_star
+
+
+class N_Puzzle_Game:
+
+    def __init__(self, dimension):
+        self.dimension = dimension
+        N_Puzzle_Game.board_positions(self)
+        N_Puzzle_Game.goal_state(self)
+        N_Puzzle_Game.board_sides(self)
+
+    def board_positions(self):
+        self.board_positions = {}
+        index = 0
+        for i in range(self.dimension):
+            for j in range(self.dimension):
+                self.board_positions[index] = [i, j]
+                index += 1
+        return self.board_positions
+
+    def goal_state(self):
+        self.goal_state = []
+        for i in range(self.dimension * self.dimension):
+            self.goal_state.append(i)
+        return self.goal_state
+
+    def board_sides(self):
+        self.sides = {}
+        squares = self.dimension * self.dimension
+        self.sides["up"] = list(range(self.dimension))
+        self.sides["down"] = list(range(squares - self.dimension, squares))
+        self.sides["left"] = []
+        for i in range(self.dimension):
+            self.sides["left"].append(i * self.dimension)
+        self.sides["right"] = []
+        for i in range(self.dimension):
+            self.sides["right"].append(i * self.dimension + self.dimension - 1)
+        return self.sides
+
+    def allowed_movements(self, state):
+        allowed = []
+        index = state.index(0)
+        if index not in self.sides["up"]:
+            allowed.append("up")
+        if index not in self.sides["down"]:
+            allowed.append("down")
+        if index not in self.sides["left"]:
+            allowed.append("left")
+        if index not in self.sides["right"]:
+            allowed.append("right")
+        return allowed
+
+    def new_state(self, state, action):
+        new_state = state[:]
+        index = state.index(0)
+        displacement = None
+        if action == "up":
+            displacement = - self.dimension
+        if action == "down":
+            displacement = self.dimension
+        if action == "left":
+            displacement = -1
+        if action == "right":
+            displacement = 1
+        new_state[index + displacement], new_state[index] = new_state[index], new_state[index + displacement]
+        return new_state
+
+    def heuristic(self, state):
+        board_pos = self.board_positions
+        distance = 0
+        for index, number in enumerate(state):
+            # print(board_pos[index], board_pos[number])
+            distance += abs(board_pos[index][0] - board_pos[number][0]) + abs(board_pos[index][1] - board_pos[number][1])
+        return distance
+
+    def resolve_bfs(self, initial_state):
+        return breadth_first_search(initial_state, self.goal_state, self)
+
+    def resolve_a_star(self, initial_state):
+        return a_star(initial_state, self.goal_state, self)
+
+    def expand_node(self, node):
+        """Returns a list of expanded nodes"""
+        expanded_nodes = []
+        movements = N_Puzzle_Game.allowed_movements(self, node.state)
+        for move in movements:
+            expanded_nodes.append(Node(N_Puzzle_Game.new_state(self, node.state, move), node, move, node.depth + 1, node.cost + 1))
+        return expanded_nodes
